3af8221e250c8e061a5a0710ac39458022e4dbdb
Philipp Burckhardt <pburckhardt@outlook.com>
2013-06-23 16:57:13 +0100

    boost commit

---
+++ boost.cpp
@@ -0,0 +1,19 @@
+#include <RcppArmadillo.h>
+// [[Rcpp::depends("RcppArmadillo")]]
+
+#include <boost/regex.hpp>
+#include <boost/foreach.hpp>
+#include <boost/range.hpp>
+
+using namespace Rcpp;
+using namespace std;
+using namespace boost;
+
+#include <iostream>
+#include <string>
+
+// [[Rcpp::export]]
+int main()
+{
+
+}
